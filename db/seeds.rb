9c0b3c4f3fca5a7c2dc5a31509041061e81b691e
MaxMillington <maxmillington89@gmail.com>
2016-01-06 15:52:52 -0700

    add three instruments and padding to instruments index page

--- db/seeds.rb
+++ db/seeds.rb
@@ -15,3 +15,23 @@ Maker.create(name: "Epiphone")
 Maker.create(name: "Roland")
 Maker.create(name: "Guild")
 Maker.create(name: "Gretch")
+
+Instrument.create(model: "Custom Shop Historic Les Paul Standard 1960 Reissue", maker_id: 1, year: 2003, style_id: 3, image_url: "2003.jpg",
+                  description: "This guitar was my main guitar for 12 years. It was purchase from the now out of business 'Best Bet Music' located in Northeastern Pennsylvania.
+                  I had been playing my first electric, the Les Paul Special purchased by my father, for two years, and I decided that I was getting good enough to warrant an upgrade.
+                  I had been saving my money for a few years, and the original plan was to purchase a black Fender Stratocaster simultaneously with a Gibson USA Les Paul Custom. At the time,
+                  I was unaware of the distinction between Gibson USA and Gibson Custom Shop, so I assumed that in purchasing a Gibson USA Les Paul Custom, I would be getting the best new Les Paul money could buy.
+                  I was inspired to buy the Les Paul Custom after having the opportunity to play a vintage one from the 70s owned by a friend of my uncle's. I expected the whole purchase to cost me around $3,500 between the Les Paul and the Strat.
+                  It was then that a friend of mine told me about a 1959 Reissue at Best Bet Music. That guitar was the first Gibson Custom Shop Les Paul I played. Not only did I immediately notice that the quality was superior to the Gibson USA models,
+                  but I also preferred the look of the guitar. I was about ready to purchase the '59 until the store owner told me about the distinctions between the '58, '59, and '60 necks. When I discovered that the '60 had a slimmer neck, I decided to custom order a '60 instead.
+                  Again, I had been used to the feel of my Les Paul Special, which had a very slim neck. Moreover, I had then, and have now, small hands, so I figured a slimmer neck would better suit my playing style. The final nail in the coffin was that I read that Jimmy Page shaved
+                  the neck of his '59 down specifically because he liked the slim-taper, so I figured if it was good enough for him it was good enough for me. Nowadays, I still prefer a slimmer neck for any guitar that is going to be a 'main guitar' of mine, but I am perfectly happy to
+                  play a chunky neck too. I think that the 2003 '59s are slightly more valuable than the '60s, but I still think I made the right choice for my playing style. Of course, if it hasn't already been discerned, the fact that I ordered my guitar in 2003 turned out to be incredibly lucky.
+                  Die-hard Les Paul fans know that the original bursts all had Brazilian Rosewood fretboards (ironically considered less luxurious than the Ebony board of the Les Paul Customs), which is now illegal to import. Unless you are willing to pay exorbitant prices for it).
+                  The one exception to this was the 2003 Custom Shop Historic Les Pauls, and I just happened to get one. Gibson made far fewer '60s than '59s or '58s that year. In fact, the Gibson Custom Shop
+                  churned out just 162 1960 reissues, and of these around 60 did not contain Brazilian Rosewood fretboards. As you can imagine, I feel incredibly lucky to have grabbed one of these guitars, especially since at the time I didn't know or care
+                  about the merits of Brazilian Rosewood. I can't remember exactly how much this guitar costs, but I do remember that I spent literally every penny I had on it. Moreover, since Gibson's reintroduction of Brazilian was so shortlived,
+                  this guitar has become a relatively sought after modern Gibson, not that I'm interested in selling mine.")
+
+Instrument.create(model: "Custom Shop Collector's Choice #18, the 'Dutchburst'", maker_id: 1, year: 2014, style_id: 3, image_url: "small_dutch.jpg" )
+Instrument.create(model: "Les Paul Special", maker_id: 1, year: 2001, style_id: 3, image_url: "special.jpg" )
